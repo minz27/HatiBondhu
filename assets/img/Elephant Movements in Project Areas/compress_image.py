abc4f477dae120d582afd46d3522d24537ec5ab0
Mreenav Deka <mreenav@gmail.com>
2021-11-13 10:30:43 +0100

    gallery part 1

--- assets/img/Elephant Movements in Project Areas/compress_image.py
+++ assets/img/Elephant Movements in Project Areas/compress_image.py
@@ -1,17 +1,23 @@
-import os
-import glob
-import PIL
 from PIL import Image
+import os
 
-def compress_images():
-	images = [file for file in os.listdir() if file.lower().endswith(('jpg', 'png', 'jpeg' ))]
-	for image in images:
-		if image == "hec.jpg":
-			continue
-		img = Image.open(image)
-		img.save(image, optimize=True,quality=60)
+path = ""
+resize_ratio = 0.5  # where 0.5 is half size, 2 is double size
 
-if __name__ == '__main__':
-	compress_images()
+def resize_aspect_fit():
+    dirs = os.listdir()
+    for item in dirs:
+        if item == 'compress_image.py':
+            continue
+        if os.path.isfile(path+item):
+            image = Image.open(path+item)
+            file_path, extension = os.path.splitext(path+item)
 
-		
+            new_image_height = int(image.size[0] / (1/resize_ratio))
+            new_image_length = int(image.size[1] / (1/resize_ratio))
+
+            image = image.resize((new_image_height, new_image_length), Image.ANTIALIAS)
+            image.save(file_path + "_small" + extension, 'JPEG', quality=90)
+
+
+resize_aspect_fit()
